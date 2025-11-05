28de3a1b0058a2b35494e7ab664c7ec22a77e334
idn-fahmifn <fahmifn.idn@gmail.com>
2025-11-05 14:29:57 +0700

    3.4

---
+++ javascript/app.js
@@ -0,0 +1,53 @@
+// variabel dengan let.
+// 1. Untuk data bisa diubah (reusable);
+
+console.log("hari ini saya belajar js");
+
+let pelajaran = "Hari ini saya sedang belajar javascript";
+console.log(pelajaran); //awal data
+
+pelajaran = "Javascript asik bukan?";
+console.log(pelajaran); // data yang sudah diubah.
+
+// variable dengan const
+const makan = "Saya tiap hari makan 3x";
+console.log(makan);
+
+
+// Tipe data string
+// tipe data string bisa menggunakan '', "", ``
+let sapa = "Halo selamat, ";
+let waktu = 'Pagi';
+let ucapan = `Ayo kita ucapkan ${sapa} ${waktu}, kepada semua tamu` //template literal
+
+// hallo selamat, pagi
+console.log(ucapan);
+
+// Tipe data number
+// mewakili angka bulat, desimal, +/-infinity, Nan
+
+let umur = 0.5
+let usia = 50
+console.log(usia-sapa);
+
+let isMakan = true //boolean benar.
+//false = 0, true = 1
+console.log(isMakan + waktu);
+
+ //tipe data undifined
+//  wajib menggunakan let
+let contoh
+console.log(contoh);
+
+// tipe data null
+let kosong  = null
+console.log(kosong + isMakan)
+
+
+
+
+
+
+
+
+
